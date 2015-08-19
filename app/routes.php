839f6a4baf0fdeccfe644aaf87ab6ba538fce35f
dgcollier <david@gcollier.me>
2015-08-19 11:39:46 -0500

    Routing and views exercises complete.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,41 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+// SAY HELLO
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "Chris") {
+        return Redirect::to('/');
+    } else {
+    	$data = array('name' => $name);
+        return View::make('my-first-view')->with($data);
+    }
+});
+
+// RESUME
+Route::get('/resume', function()
+{
+	return 'This is my resume.';
+});
+
+// PORTFOLIO
+Route::get('/portfolio', function()
+{
+	return 'This is my portfolio.';
+});
+
+// ROLLDICE
+Route::get('/rolldice/{userGuess}', function($userGuess)
+{
+	$compGuess = mt_rand(1,6);
+
+	if ($userGuess == $compGuess) {
+		$message = 'You WIN!';
+	} else {
+		$message = 'You LOSE!';
+	} 
+
+	$data = array('compGuess' => $compGuess, 'userGuess' => $userGuess, 'message' => $message);
+	return View::make('rolldice')->with($data);
+});
