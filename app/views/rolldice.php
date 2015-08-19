b75244c5ae8a3f80c2e31fe7fea3707c3bd82659
dgcollier <david@gcollier.me>
2015-08-19 11:40:56 -0500

    Rolldice exercise complete.

---
+++ app/views/rolldice.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rolldice</title>
+</head>
+<body>
+	<h1>Roll Dice Game:</h1>
+    <h2>The computer rolled <?= $compGuess; ?>!</h2>
+    <h2>You rolled <?= $userGuess ?>!</h2>
+
+    <h3><?= $message ?></h3>
+</body>
+</html>
