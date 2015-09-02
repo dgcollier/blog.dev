af6ce8c86c2a80d720dd1c8bca87ed4b05341ac2
dgcollier <david@gcollier.me>
2015-09-02 15:07:49 -0500

    changed header backgrounds

--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('header')
     <!-- Page Header -->
-    <header class="intro-header" style="background-image: url('/img/blog_post.jpg')">
+    <header class="intro-header" style="background-image: url('/img/blog_profile.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
